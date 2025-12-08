1e0c400d33346d456c8f9021d30904070ef8407b
Martí Carreras <edu@applicationsdev.com>
2025-12-08 14:01:22 +0100

    Afegida funcionalitat ....

---
+++ static/js/script.js
@@ -0,0 +1,11 @@
+
+window.addEventListener(
+	'load',
+	function () {
+		let body = document.querySelector('body');
+		let header = document.createElement('h1');
+		header.innerHTML = 'ModaGlobal';
+		body.append(header);
+	}
+);
+
